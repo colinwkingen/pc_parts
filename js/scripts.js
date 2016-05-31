2651a5217e0cfd6d65cba25c3c4356350df73a0c
Colin W Kingen and Tony Fabery <colin@>
2016-05-31 13:26:10 -0700

    add js to change header alignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(function() {
   $('.clickable').click(function() {
-    $('.initially-showing').fadeToggle();
-    $('.initially-hidden').fadeToggle();
+    $('.initially-showing').toggle().fadeIn();
+    $('.initially-hidden').toggle().fadeOut();
+  });
+
+  $('button#left').click(function() {
+    $('h1').removeClass();
+    $('h1').addClass("header-left");
+  });
+
+  $('button#right').click(function() {
+    $('h1').removeClass();
+    $('h1').addClass("header-right");
+  });
+
+  $('button#center').click(function() {
+    $('h1').removeClass();
+    $('h1').addClass("header-center");
   });
 });
